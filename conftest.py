8a284151dc266248b5cc584f550d90a605ff9523
Roman Burlaka <burlakarv@gmail.com>
2025-10-19 21:04:10 +0200

    Refactor  ci.yaml

--- conftest.py
+++ conftest.py
@@ -1,3 +1,7 @@
+import shutil
+import tempfile
+
+import os
 
 import pytest
 from selenium import webdriver
@@ -7,16 +11,38 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture(scope="function")
 def driver():
-    # === BEFORE test ===
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--incognito")
-    chrome_options.add_argument("--window-size = 1400, 1080")
-    service_properties = Service(executable_path=ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service_properties, options=chrome_options)
+    chrome_options = webdriver.ChromeOptions()  # <-- исправлено
+
+    # Check run in CI (GitHub Actions)
+    is_ci = os.environ.get("CI") == "true"
+
+    if is_ci:
+        # === For CI ===
+        chrome_options.add_argument("--headless=new")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--window-size=1920,1080")
+
+        # uniq profile
+        user_data_dir = tempfile.mkdtemp()
+        chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
+    else:
+        # === Local run ===
+        chrome_options.add_argument("--incognito")
+        chrome_options.add_argument("--window-size=1400,1080")
+
+    # Create driver
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=chrome_options)
 
     yield driver
 
     driver.quit()
+
+    if is_ci:
+        shutil.rmtree(user_data_dir, ignore_errors=True)
+
 
  # === AFTER test ===
 @pytest.hookimpl(hookwrapper=True, tryfirst=True)
